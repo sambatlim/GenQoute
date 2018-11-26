87d2af3be3695264ca96fcd910341c0cfcd35fd5
Sambat Lim <sambatlim86@gmail.com>
2018-11-26 20:23:23 +0700

    Update splitWord.js

--- splitWord.js
+++ splitWord.js
@@ -1,6 +1,9 @@
-var fs = require("fs");
-var text = fs.readFileSync("./12.txt").toString("utf-8");
+function split() {
+  var fs = require("fs");
+  var text = fs.readFileSync("./12.txt").toString("utf-8");
 
-var textByLine = text.split(".");
-var i = Math.floor(Math.random() * textByLine.length);
-console.log(textByLine[i]);
+  var textByLine = text.split(".");
+  var i = Math.floor(Math.random() * textByLine.length);
+  console.log(textByLine[i]);
+}
+
