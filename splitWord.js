27455d006c0f71055731300142e0cdfa87da5cbc
Sambat Lim <sambatlim86@gmail.com>
2018-11-26 20:17:11 +0700

    Add files via upload

---
+++ splitWord.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+var text = fs.readFileSync("./12.txt").toString("utf-8");
+
+var textByLine = text.split(".");
+var i = Math.floor(Math.random() * textByLine.length);
+console.log(textByLine[i]);
